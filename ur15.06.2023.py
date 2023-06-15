71a1d3016e8c794eb16ec4d4538b1cc99dbe69cd
rtbn12 <136724287+rtbn12@users.noreply.github.com>
2023-06-16 00:10:25 +0300

    Add files via upload

--- ur15.06.2023.py
+++ ur15.06.2023.py
@@ -1,14 +1,19 @@
-def strcounter(s):
-    for sym in set(s) :
-        count=0
-        for sub_sym in s:
-            if sym==sub_sym:
-                count +=1
-        print(f'{sym}-{count}')
+# def strcounter(s):
+#     for sym in set(s) :
+#         count=0
+#         for sub_sym in s:
+#             if sym==sub_sym:
+#                 count +=1
+#         print(f'{sym}-{count}')
+#
+# strcounter('aaaaaaaaaaassssssssssssdgvknuhgeuhgwh')
 
-strcounter('aaaaaaaaaaassssssssssssdgvknuhgeuhgwh')
-
-
+slovo = str(input())
+a = slovo[::-1]
+if slovo == a:
+  print("True")
+else:
+  print("False")
 
 
 
