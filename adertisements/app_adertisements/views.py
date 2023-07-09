c7a7e1e9f05bf3f7591684b90b6a1445b8b64d4a
rtbn12 <136724287+rtbn12@users.noreply.github.com>
2023-07-09 13:08:01 +0300

    Add files via upload

---
+++ adertisements/app_adertisements/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):return HttpResponse('Успех от оуспеха для успеха при успехе!!!!!!!!!!!!!!!!!!!')
+
+
+
+
+
+
+
+
+
+
+
+
+
