64f6b8dfc165e8119b624efb3395458c89dbda83
rtbn12 <136724287+rtbn12@users.noreply.github.com>
2023-07-23 16:28:27 +0300

    Add files via upload

--- adertisements/app_adertisements/views.py
+++ adertisements/app_adertisements/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
-def index(request):return HttpResponse('Успех от оуспеха для успеха при успехе!!!!!!!!!!!!!!!!!!!')
+
+def index(request):return render(request,'index.html')
+
+
+
+def top_sellers(request):return render(request,'top-sellers.html')
 
 
 
