a1cb57ad9d07c7cc58dadad1340a6c13c0a3833f
rtbn12 <136724287+rtbn12@users.noreply.github.com>
2023-08-18 17:43:35 +0300

    Add files via upload

--- adertisements/app_adertisements/admin.py
+++ adertisements/app_adertisements/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Advertisment
 
-# Register your models here.
+
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display =['id','title','description','price','auction','created_date']
+    list_filter = ['price','auction','create_at']
+    actions = ['make_auction_as_true','make_auction_as_false']
+    fieldsets = (
+        ('Общие',{
+            'fields':('title','description')
+        }),
+        ('Финанасы',{
+            'fields':('price','auction'),
+            'classes':['collapse']
+        })
+    )
+
+
+    @admin.action(description='Добавить возможность торга')
+    def make_auction_as_true(self,request,queryset):
+        queryset.update(auction=True)
+
+    @admin.action(description='Убрать возможность торга')
+    def make_auction_as_false(self, request, queryset):
+        queryset.update(auction=False)
+
+
+admin.site.register(Advertisment,AdvertisementAdmin)
--- adertisements/app_adertisements/models.py
+++ adertisements/app_adertisements/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
+from django.utils import timezone
 
-# Create your models here.
+class Advertisment(models.Model):
+    title=models.CharField('Название',max_length=128)
+    description=models.TextField('Описание')
+    price=models.DecimalField('Цена',max_digits=10,decimal_places=3)
+    auction = models.BooleanField('Торг',help_text='Укажите,если возможен торг')
+    create_at= models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        if self.create_at.date()==timezone.now().date():
+            created_time=self.create_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color:green;font-weight: bold;">Сегодня в {}</span>',created_time
+            )
+        return self.create_at.strftime('%d.%m.%Y')
+
+    @admin.display(description='Дата обновления')
+    def updated_date(self):
+        if self.updated_at.date()==timezone.now().date():
+            updated_time =self.updated_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color:green;font-weight: bold;">Сегодня в {}</span>',updated_time
+            )
+        return self.updated_at.strftime('%d.%m.%Y')
+
+
+
+    def __str__(self):
+        return f'Advertisement(id={self.id},title={self.title},price={self.price})'
+
+    class Meta:
+        db_table='advertisements'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
